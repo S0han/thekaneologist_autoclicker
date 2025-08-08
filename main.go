3e9dac95535340f28f3faaacfd6eafc14d1c7869
S0han <kane.pereira18@gmail.com>
2025-08-08 00:40:30 -0400

    update: link the keyboard button press to simulated mouseclick

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 
 	"atomicgo.dev/keyboard"
 	"atomicgo.dev/keyboard/keys"
+	"github.com/go-vgo/robotgo"
 )
 
 func main() {
 	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
 		if key.Code == keys.Enter {
-			fmt.Println("Enter key pressed script will terminate.")
-			return true, nil
+			robotgo.Click("left", true)
+			fmt.Println("Hitting Enter triggers a simulated mouse-click event!")
+			return false, nil
 		}
 
 		fmt.Println("\r" + key.String())
-		return false, nil
+		return true, nil
 	})
 }
