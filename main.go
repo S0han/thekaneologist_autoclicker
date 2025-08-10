d23c47e1697c3e5dcc4b5b6e5a055cc069fae948
S0han <kane.pereira18@gmail.com>
2025-08-10 11:26:40 -0400

    update: add cotrols to start/stop program with goroutines

--- main.go
+++ main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"atomicgo.dev/keyboard"
 	"atomicgo.dev/keyboard/keys"
 	"github.com/go-vgo/robotgo"
 )
 
-func main() {
-	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
-		if key.Code == keys.Enter {
+func autoClicker(running *bool) {
+	for {
+		if *running {
 			robotgo.Click("left", true)
-			fmt.Println("Hitting Enter triggers a simulated mouse-click event!")
+			fmt.Println("Simulated mouse-click event!")
+			time.Sleep(10 * time.Second)
+			fmt.Println("next click after 10-sec")
+		} else {
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+}
+
+func main() {
+	running := false
+
+	go autoClicker(&running)
+
+	keyboard.Listen(
+		func(key keys.Key) (stop bool, err error) {
+			if key.String() == "S" {
+				fmt.Println("Hitting 'S' STARTS the auto-click")
+				running = true
+			} else if key.String() == "T" {
+				fmt.Println("Hitting 'T' TERMINATES the auto-click")
+				running = false
+			} else if key.String() == "E" {
+				fmt.Println("Ended Script by pressing 'E'")
+				return true, nil
+			} else {
+				fmt.Println("\r" + key.String())
+				fmt.Println("Unrelated key pressed")
+			}
 			return false, nil
-		}
-
-		// logs the key presses
-		fmt.Println("\r" + key.String())
-
-		//terminate script with this button
-		if key.String() == "P" {
-			fmt.Println("STOP key pressed!")
-			return true, nil
-		}
-
-		fmt.Println("Un-related key pressed")
-		return false, nil
-	})
-	//use this timer feature for the intervals
-	// time.Sleep(10 * time.Second)
+		})
 }
