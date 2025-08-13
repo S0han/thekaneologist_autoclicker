299899ef58e1831595d79f649614475d788c7227
S0han <kane.pereira18@gmail.com>
2025-08-13 03:34:32 -0400

    update: dynamic inputs and prevent race conditions with mutexes

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"sync"
 	"time"
 
 	"atomicgo.dev/keyboard"
@@ -14,32 +16,80 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-func autoClicker(running *bool) {
+func autoClicker(running *bool, interval *time.Duration, mu *sync.Mutex) {
 	for {
-		if *running {
+		mu.Lock()
+		copy_running := *running
+		mu.Unlock()
+		if copy_running {
+
 			robotgo.Click("left", true)
 			fmt.Println("Simulated double-click mouse event!")
 
-			fmt.Println("next click after 3-sec")
-			time.Sleep(3 * time.Second)
+			fmt.Printf("next click after %v", interval)
+			time.Sleep(*interval * time.Second)
 		} else {
 			time.Sleep(100 * time.Millisecond)
 		}
 	}
 }
 
-func autoClickerGUI() {
+func autoClickerGUI(running *bool, s *string, t *string, interval *time.Duration, mu *sync.Mutex) {
 	a := app.New()
 	w := a.NewWindow("")
 
+	//start input
 	start_ac_key_label := widget.NewLabel("Start:")
-	stop_ac_key_label := widget.NewLabel("Stop:")
-	interval_label := widget.NewLabel("Interval:")
 	start_ac_key_input := widget.NewEntry()
+	start_ac_key_input.OnChanged = func(text string) {
+		if len(text) == 1 {
+			mu.Lock()
+			*s = text
+			mu.Unlock()
+			return
+		}
+	}
+
+	//stop input
+	stop_ac_key_label := widget.NewLabel("Stop:")
 	stop_ac_key_input := widget.NewEntry()
+	stop_ac_key_input.OnChanged = func(text string) {
+		if len(text) == 1 {
+			mu.Lock()
+			*t = text
+			mu.Unlock()
+			return
+		}
+	}
+
+	//time interval
+	interval_label := widget.NewLabel("Interval:")
 	interval_input := widget.NewEntry()
-	start_button := widget.NewButton("START", func() {})
-	stop_button := widget.NewButton("STOP", func() {})
+	interval_input.OnChanged = func(text string) {
+		//convert the string input to integer
+		sec, err := strconv.Atoi(text)
+		if err != nil {
+			return
+		}
+
+		mu.Lock()
+		*interval = time.Duration(sec) * time.Second
+		mu.Unlock()
+	}
+
+	//start & stop buttons
+	start_button := widget.NewButton("START", func() {
+		mu.Lock()
+		fmt.Println("Hitting 'START' button STARTS the auto-click ")
+		*running = true
+		mu.Unlock()
+	})
+	stop_button := widget.NewButton("STOP", func() {
+		mu.Lock()
+		fmt.Println("Hitting 'STOP' button TERMINATES the auto-click ")
+		*running = false
+		mu.Unlock()
+	})
 
 	w.SetContent(container.NewVBox(
 		start_ac_key_label,
@@ -57,23 +107,35 @@ func autoClickerGUI() {
 
 func main() {
 	running := false
+	var mu sync.Mutex
+	var interval time.Duration = 3 * time.Second
+	s := ""
+	t := ""
 
-	go autoClicker(&running)
+	go autoClicker(&running, &interval, &mu)
 
 	go keyboard.Listen(
 		func(key keys.Key) (stop bool, err error) {
-			if key.String() == "s" {
-				fmt.Println("Hitting 's' STARTS the auto-click")
+			mu.Lock()
+			copy_s := s
+			copy_t := t
+			mu.Unlock()
+
+			if key.String() == copy_s {
+				mu.Lock()
+				fmt.Printf("Hitting '%v' STARTS the auto-click", copy_s)
 				running = true
-			} else if key.String() == "t" {
-				fmt.Println("Hitting 't' TERMINATES the auto-click")
+				mu.Unlock()
+			} else if key.String() == copy_t {
+				mu.Lock()
+				fmt.Printf("Hitting '%v' TERMINATES the auto-click", copy_t)
 				running = false
+				mu.Unlock()
 			} else {
 				fmt.Println("\r" + key.String())
 				fmt.Println("Unrelated key pressed!")
 			}
 			return false, nil
 		})
-
-	autoClickerGUI()
+	autoClickerGUI(&running, &s, &t, &interval, &mu)
 }
